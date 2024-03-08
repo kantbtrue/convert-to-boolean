7f03d9912835d4da74e00b0ace1183b42450de43
Shashikant Yadav <shashikant1989yadav@gmail.com>
2024-03-08 17:53:48 +0530

    Fix: String and infinity values check

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 /**
- * Convert any value into a boolean value.
- *
- * @param {*} val A mixed value.
+ * Testing the number is a float number.
  */
-function convertToBoolean(val) {
+function isFloatNumber(num) {
+    return Number(num) === num && num % 1 !== 0;
+}
+
+/**
+ * Convert any valueue into a boolean valueue.
+ *
+ * @param {*} value A mixed valueue.
+ */
+function convertToBoolean(value) {
     let result;
-    const valType = typeof val;
-    if ("boolean" === valType) {
-        result = val;
+    const valueType = typeof value;
+    if ("boolean" === valueType) {
+        result = value;
     } else {
-        switch (valType) {
+        switch (valueType) {
             case "undefined":
             case "null":
             case "NaN":
                 result = false;
                 break;
             case "object":
-                if (Object.is(val, null)) {
+                if (Object.is(value, null)) {
                     result = false;
-                } else if (Array.isArray(val)) {
-                    if (0 === val.length) {
+                } else if (Array.isArray(value)) {
+                    if (0 === value.length) {
                         result = false;
                     } else {
                         result = true;
                     }
-                } else if (0 === Object.keys(val).length) {
+                } else if (0 === Object.keys(value).length) {
                     result = false;
                 } else {
                     result = true;
@@ -32,27 +39,47 @@ function convertToBoolean(val) {
                 break;
             case "number":
             case "string":
-                if ("string" === valType) {
-                    if (Object.is(parseInt(val), NaN)) {
-                        return true;
+                if ("string" === valueType) {
+                    if (0 === value.length) {
+                        return false;
                     } else {
-                        if (parseInt(val) === 0) {
-                            return false;
-                        } else {
+                        if (Object.is(parseInt(value), NaN)) {
                             return true;
                         }
+
+                        if (0 === parseInt(value)) {
+                            return false;
+                        }
+
+                        return true;
                     }
                 }
-                if (0 === parseInt(val)) {
+
+                if (!Number.isFinite(value)) {
+                    return false;
+                }
+
+                if (isFloatNumber(value)) {
+                    return value > 0;
+                }
+
+                if (0 >= Number(value)) {
                     result = false;
                 } else {
                     result = true;
                 }
+
                 break;
             case "bigint":
             case "symbol":
-            case "function":
                 result = true;
+                break;
+            case "function":
+                if ("undefined" === typeof value()) {
+                    result = false;
+                } else {
+                    result = true;
+                }
                 break;
         }
     }
--- test/index.test.js
+++ test/index.test.js
@@ -1,52 +1,58 @@
 import convertToBoolean from "../src/index.js";
 import { describe, it } from "mocha";
 import { expect } from "chai";
-describe("Helper Funcitons", () => {
+describe("Check", () => {
     describe("Convert any value to boolean", () => {
         function undefinedFunc() {
             return;
         }
-        let values = {
-            boolean: true,
-            "empty array": [],
-            "empty object": {},
-            "empty string": "",
-            NaN: "minus" - 1,
-            null: null,
-            undefined: undefined,
-            bigint: 9007199254740995n,
-            symbol: Symbol("test"),
-            "numric string - false": "0",
-            "numric string - true": "1",
-            "simple array": ["a", "b"],
-            "simple object": { a: 1, b: 2 },
-            string: "string",
-            infinite: 2 / 0,
-            integer: 1,
-            float: 0.1,
-            "undefined function": undefinedFunc,
-            function: () => {
-                return true;
-            },
-        };
-        Object.keys(values).forEach((key) => {
-            it(`Should return a boolean from ${key} value`, () => {
-                expect(convertToBoolean(values[key])).to.be.a("boolean");
-            });
+        it("Should return `true` for valid function", () => {
+            expect(
+                convertToBoolean(() => {
+                    return true;
+                })
+            ).to.equal(true);
+        });
+        it("Should return `false` for undefined function", () => {
+            expect(convertToBoolean(undefinedFunc)).to.equal(false);
+        });
+        it("Should return `true` for truthy values like 1, non empty string, true, none empty array and object", () => {
+            expect(convertToBoolean(1)).to.equal(true);
+            expect(convertToBoolean(true)).to.equal(true);
+            expect(convertToBoolean("kanbtrue")).to.equal(true);
+            expect(convertToBoolean(["a", "b"])).to.equal(true);
+            expect(convertToBoolean({ a: "b", c: "d" })).to.equal(true);
+        });
+        it("Should return `false` for unwanted values like 0, -1, empty [], empty {}, false, empty string, NaN, null, undefined", () => {
+            expect(convertToBoolean(false)).to.equal(false);
+            expect(convertToBoolean(0)).to.equal(false);
+            expect(convertToBoolean(-1)).to.equal(false);
+            expect(convertToBoolean("")).to.equal(false);
+            expect(convertToBoolean([])).to.equal(false);
+            expect(convertToBoolean({})).to.equal(false);
+            expect(convertToBoolean(1 / 0)).to.equal(false); // Infiniy
+            expect(convertToBoolean("kantbtrue" - 1)).to.equal(false); // NaN
+            expect(convertToBoolean(null)).to.equal(false);
+            expect(convertToBoolean(undefined)).to.equal(false);
         });
     });
-    describe("Check numeric string and number values", () => {
+    describe("Check numeric string and floating values", () => {
         it("Should return `true` for numeric string value 1", () => {
             expect(convertToBoolean("1")).to.equal(true);
         });
         it("Should return `false` for numeric string value 0", () => {
             expect(convertToBoolean("0")).to.equal(false);
         });
-        it("Should return `true` for numeric value 1", () => {
-            expect(convertToBoolean(1)).to.equal(true);
+        it("Should return `true` for bigint value", () => {
+            expect(convertToBoolean(9007199254740995n)).to.equal(true);
         });
-        it("Should return `false` for numeric value 0", () => {
-            expect(convertToBoolean(0)).to.equal(false);
+        it("Should return `true` for float values greater than 0", () => {
+            expect(convertToBoolean(0.1)).to.equal(true);
+            expect(convertToBoolean(0.01)).to.equal(true);
+        });
+        it("Should return `false` for float values smaller than or equal to 0", () => {
+            expect(convertToBoolean(0.0)).to.equal(false);
+            expect(convertToBoolean(-0.0)).to.equal(false);
         });
     });
 });
